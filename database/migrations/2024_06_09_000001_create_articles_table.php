834179b839e8bb45c1a13df99966c5d26faed12c
nozaki <ysms.nzk.11@gmail.com>
2025-07-27 23:47:33 +0900

    最新化

--- database/migrations/2024_06_09_000001_create_articles_table.php
+++ database/migrations/2024_06_09_000001_create_articles_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->tinyInteger('category_id');
             $table->string('title');
             $table->tinyInteger('category_id');
             $table->text('content');
