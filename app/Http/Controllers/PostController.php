bf62d63ada31cc8c9eb3f036a4f885924dd01016
keiichi kato <momonomono@gmail.com>
2025-08-12 22:06:20 +0900

    controllerにコメントを書き足しました

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,22 +10,39 @@ use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
+
+    /**
+     * 新規投稿画面
+     * 
+     * @param 
+     * @return
+     */
     public function createArticle()
     {
+        // カテゴリーのセレクター用データ
         $categories = Category::cases();
 
         return view("create")
             ->with(compact("categories"));
     }
 
-    public function storeArticle(ArticleStoreRequest $request)
+    /**
+     * 新規投稿登録
+     * 
+     * @param ArticleRequest $request
+     * @return
+     */
+    public function Article(ArticleStoreRequest $request)
     {   
+        // DBに入れるデータを変数に挿入
         $todolists = $request->validated();
         $todolists['image_path'] = $request->file('image_path')->store('images', 'public');
         $todolists['user_id'] = Auth::id();
 
+        // DBに登録
         Article::create($todolists);
 
+        // トップ画面に戻る
         return redirect()
             ->route('top');
     }
