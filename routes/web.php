95c861a078231d3cb1e48118f6a65a3ca0ffeb64
lexushiro0530 <sayasaki0000@gmail.com>
2025-08-20 15:11:18 +0900

    use文被りを消去

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
