d9926e1bdde02f64af05ef85aba9053292bddf74
lexushiro0530 <sayasaki0000@gmail.com>
2025-08-23 23:36:15 +0900

    メニューバーとヘッダー変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,13 +2,25 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('top') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
+            <div class="flex items-center">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="shrink-0 mx-4 flex items-center">
+                        <a href="{{ route('top') }}">
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        </a>
+                    </div>
                 </div>
+
+                <div class="hidden sm:flex">
+                    <!--  -->
+                    <div class="shrink-0 mx-4 flex items-center">
+                        <a href="{{ route('post.myPosts') }}">
+                            <p>myPosts</p>
+                        </a>
+                    </div>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
@@ -78,6 +90,10 @@
                 <div class="mt-3 space-y-1">
                     <x-responsive-nav-link :href="route('profile.edit')">
                         {{ __('Profile Information') }}
+                    </x-responsive-nav-link>
+
+                    <x-responsive-nav-link :href="route('post.myPosts')">
+                        <p>myPosts</p>
                     </x-responsive-nav-link>
 
                     <!-- Authentication -->
